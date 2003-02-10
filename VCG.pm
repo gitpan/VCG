1c8fd57ca1ede233dce3ceccef837955191d22be
Aaron James Trevena <teejay@cpan.org>
2003-02-10 23:14:41 +0000

    Import of TEEJAY/VCG-0.2 from CPAN.
    gitpan-cpan-distribution: VCG
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         TEEJAY/VCG-0.2.tar.gz
    gitpan-cpan-author:       TEEJAY
    gitpan-cpan-maturity:     released

--- VCG.pm
+++ VCG.pm
@@ -1,13 +1,13 @@
 package VCG;
 
 use strict;
-use vars qw($AUTOLOAD $VERSION);
+use vars qw($AUTOLOAD $VERSION $DEBUG $program);
 
-use Carp;
 use IPC::Run qw(run);
 
-$VERSION = '0.1';
-
+$VERSION = '0.2';
+$DEBUG = 0;
+$program = "xvcg";
 
 =head1 NAME
 
@@ -24,19 +24,43 @@ VCG - Interface to the VCG graphing tool
 
   $vcg->add_edge(source => 'aaa', target=>'bbb');
 
-  $vcg->as_pbm('mygraph.pbm');
+  $vcg->output_as_pbm('mygraph.pbm');
+
+  my $data = $vcg->as_ppm();
+  open (OUTFILE, 'outfile.ppm') or die "error $!\n";
+  print OUTFILE $data;
+  close OUTFILE;
 
 =head1 DESCRIPTION
 
 This module provides an interface to to the vcg graphing tool. It supports a 
-limited selection of options and file formats.
+limited selection of options and file formats. The vcg graphing tool homepage 
+is currently http://rw4.cs.uni-sb.de/users/sander/html/gsvcg1.html but is being actively
+developed elsewhere.
 
-This module is based very heavily on Leon Brocard's GraphViz module, it tries
+This module is based on Leon Brocard's GraphViz module, it tries
 to provide a similar interface to offer some sense of consistency.
+
+VCG is now in active development and although Graph::Writer::VCG already exists,
+this module provides a similar interface to graphviz and will be more closely tied
+into vcg as it becomes more actively developed - see James Micheal DuPonts announcement
+at http://mail.gnome.org/archives/dia-list/2003-February/msg00029.html.
 
 =cut
 
-my $program = "xvcg";
+=head1 METHODS
+
+=head2 new
+
+new objects are created using the constructor method 'new'.
+
+This method accepts name attributes in the form :
+
+my $vcg = VCG->new(outfile=>'foo.ps')
+my $vcg = VCG->new(title=>'Dia Dependancies Diagram',debug=>1);
+my $vcg = VCG->new();
+
+=cut
 
 sub new {
   my $class = shift;
@@ -49,12 +73,28 @@ sub new {
   $self->{nodes} = [];
   $self->{xmax} ||= 700;
   $self->{ymax} ||= 700;
-  $self->{x} ||= 30;
-  $self->{y} ||= 30;
+  $self->{'x'} ||= 30;
+  $self->{'y'} ||= 30;
   $self->{title} ||= "untitled";
+  $self->{outfile} ||= "vcg.out";
+
+  $DEBUG = 1 if ($config{debug});
 
   return $self;
 }
+
+=head2 add_egde
+
+add_edge allows you to add edges to your vcg object (edges are the lines or relationships between nodes).
+
+In a Finite State Diagram, edges would represent transitions between states.
+
+This method accepts the source, target and colour of the edge :
+
+$vcg->add_edge( source=>'from_node', target=>'to_node');
+$vcg->add_edge( source=>'aaa', target=>'bbb', color=>'grey');
+
+=cut
 
 sub add_edge {
   my $self = shift;
@@ -64,6 +104,21 @@ sub add_edge {
   push (@{$self->{edges}}, $edge);
   return 1;
 }
+
+=head2 add_node
+
+add_node allows you to add nodes to your vcg object (nodes are the things connected, while edges are the connections).
+
+In a Finite State Diagram, nodes would be the individual states.
+
+This method accepts the label, title and background colour of the node :
+
+$vcg->add_node( title=>'aaa' );
+$vcg->add_node( label=>'aaa' );
+$vcg->add_node( label=>'aaa', title=>'A', color=>'yellow' );
+
+
+=cut
 
 sub add_node {
   my $self = shift;
@@ -76,7 +131,10 @@ sub add_node {
 }
 
 
-sub get_graph {
+######################################################################################
+# generate the vcg grammar for the graph
+
+sub _get_graph {
   my $self = shift;
   my $nodes = join ("\n",@{$self->{nodes}});
   my $edges =  join ("\n",@{$self->{edges}});
@@ -90,30 +148,94 @@ end
   return $graph;
 }
 
+=head2 as_ps, as_pbm, as_ppm, as_vcg, as_plainvcg
+
+The VCG object allows you to access the output of the vcg tool directly, suitable for using with graphic libraries - although some libraries or older versions may not be able to cope with these formats.
+
+You can access the output in any of postscript, pbm, ppm, vcg (annotated) and vcg (plain) :
+
+my $image_as_ppm = $vcg->as_ppm(); # string of image as formatted as ppm
+
+
+
+my $vcg_with_coords = $vcg->as_vcg();
+
+=head2 output_as_ps, output_as_pbm, output_as_ppm
+
+The VCG object allows you to output straight to a file through the vcg tool in any of postscript, pbm and ppm. This functionality requires that the vcg tool be installed.
+
+
+
+=head2 output_as_vcg, output_as_plainvcg
+
+The VCG object also allows you to output straight to file in annotated vcg with coordinates, or plain vcg syntax. The plain syntax does not require the vcg tool to be installed.
+
+=cut
+
 ######################################################################################
 # Generate magic methods to save typing
 
 sub AUTOLOAD {
   my $self = shift;
   my $type = ref($self)
-    or croak "$self is not an object";
+    or die "$self is not an object";
 
   my $name = $AUTOLOAD;
   $name =~ s/.*://;   # strip fully-qualified portion
 
   return if $name =~ /DESTROY/;
 
-  if ($name =~ /^as_(ps|pbm|ppm)$/) {
+  if ($name =~ /^(output_)?as_(ps|pbm|ppm|vcgplain|vcg)$/) {
     my $filename = shift || $self->{config}{outfile};
-
-    my $vcg = $self->get_graph();
+    my $vcg = $self->_get_graph();
     my $output;
-    run [$program, "-$1".'output', $filename, "- "], \$vcg, \$output;
-    return;
+    my $return_file = $1;
+    my $filetype = $2;
+
+    if ($filetype eq /vcgplain/) {
+      return $vcg unless ($1);
+      open OUTFILE,">$filename" or die "couldn't open $filename for output : $!\n";
+      print OUTFILE $vcg;
+      close OUTFILE;
+      return 1;
+    }
+
+    run [$program, "-$filetype".'output', $filename, "- "], \$vcg, \$output;
+    warn $output if ($DEBUG);
+    unless ($1) {
+      open (FILE,$filename) or die "unable to open $filename : $!\n";
+      my $data = join ('',(<FILE>));
+      close FILE;
+      return $data;
+      unlink $filename or die "unable to remove tempory file $filename : $! \n";
+    }
+    return $output;
   }
-  croak "Method $name not defined!";
+  die "Method $name not defined!";
 }
 
+##########################################################################
+
+=head1 SEE ALSO
+
+  GraphViz : http://www.graphviz.org
+
+  GraphViz perl module
+
+  Graph::Writer::VCG perl module
+
+=head1 AUTHOR
+
+Aaron Trevena E<lt>F<aaron@droogs.org>E<gt>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2003, Aaron Trevena, Leon Brocard
+
+This module is free software; you can redistribute it or modify it
+under the same terms as Perl itself.
+
+=cut
 
 ##########################################################################
 
